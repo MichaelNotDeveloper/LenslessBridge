0e0f15cfbb614ad179dc7c1a3140ebca25cf6246
Миша Замешаев <mazameshaev@edu.hse.ru>
2025-09-16 08:15:21 +0300

    other configs5

--- lensless/recon/utils.py
+++ lensless/recon/utils.py
@@ -1107,7 +1107,7 @@ class Trainer:
                     )
                     
                 # -- add discriminator loss to total loss
-                if self.discrimintor is not None:
+                if self.gan_amount_of_epoch:
                     if y_pred_crop.shape[1] == 1:
                         # if only one channel, repeat for LPIPS
                         y_pred_crop = y_pred_crop.repeat(1, 3, 1, 1)
